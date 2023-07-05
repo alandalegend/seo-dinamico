49931a9d96f2748b0c6aea705a6c48ecb8df5aca
Jonathan AH <54410480+JonathanArtetaH@users.noreply.github.com>
2023-07-05 15:03:49 -0600

    Ajustes

--- src/app/pages/detalle/detalle.component.ts
+++ src/app/pages/detalle/detalle.component.ts
@@ -1,6 +1,6 @@
-import { DOCUMENT } from '@angular/common';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router'; 
 
@@ -105,30 +105,32 @@ export class DetalleComponent implements OnInit{
     private url: ActivatedRoute,
     private meta: Meta,
     private title: Title,
-    
+    @Inject(PLATFORM_ID) private platformId: Object, 
     @Inject(DOCUMENT) private doc: any, 
     ){
-      
-      this.url.queryParams.subscribe(async (e: any) =>{ 
-        this.id =  e.id;       
-         
-        
-        this.meta.addTag({name: 'robots', content:'noindex'}) 
-        const linkElement = this.doc.head.querySelector(`link[rel='canonical']`)
-        || this.doc.head.appendChild(this.doc.createElement('link'));
-        
-        if (linkElement) {
-          linkElement.setAttribute('rel', 'canonical');
-          linkElement.setAttribute('href', this.doc.URL.split('?')[0]);
-        } 
-      });
-      
     } 
+      
     result: any =[]
     id: string = '';
+    name: string = '';
     ngOnInit(): void {
-      this.setSeo('nombre.replace'+this.id,'https://rickandmortyapi.com/api/character/avatar/'+this.id+'.jpeg')  
-    this.getInfoPersonaje();
+        this.url.queryParams.subscribe(async (e: any) =>{ 
+          this.id =  e.id;       
+          this.name =  e.name;       
+           
+          
+          this.meta.addTag({name: 'robots', content:'noindex'}) 
+          const linkElement = this.doc.head.querySelector(`link[rel='canonical']`)
+          || this.doc.head.appendChild(this.doc.createElement('link'));
+          
+          if (linkElement) {
+            linkElement.setAttribute('rel', 'canonical');
+            linkElement.setAttribute('href', this.doc.URL.split('?')[0]);
+          } 
+        });
+        
+      this.setSeo(this.name, 'https://rickandmortyapi.com/api/character/avatar/'+this.id+'.jpeg')  
+        this.getInfoPersonaje();
   }
   
   ngOnDestroy(): void {
@@ -136,10 +138,20 @@ export class DetalleComponent implements OnInit{
   }
 
   setSeo(title: any ='Sin titulo', image: any  ){
+    // if(isPlatformBrowser(this.platformId)){ 
+    //     let url = window.location.href;
+    //     const params = new URLSearchParams(url);
+    //     let id = params.has('id')
+
+    //     console.log(params)
+    //     console.log(id)
+    // }
+    let ima2 = 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+
     this.title.setTitle( title +' | SEO dinamico') 
           this.meta.updateTag({ property: 'og:title', content: title +' | SeoDinamico' }); 
           this.meta.updateTag({ property: 'description', content: image }); 
-          this.meta.updateTag({ property: 'og:image', content: image});
+          this.meta.updateTag({ property: 'og:image', content: ima2});
   }
   
   async getInfoPersonaje(){ 
